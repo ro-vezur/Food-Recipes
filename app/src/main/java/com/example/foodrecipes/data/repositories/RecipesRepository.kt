76ecc3ba0f09fa90698d8996d963568a2ca7b897
ro-vezur <authenttesting@gmail.com>
2025-02-01 23:49:56 +0200

    feat: recipes db

--- app/src/main/java/com/example/foodrecipes/data/repositories/RecipesRepository.kt
+++ app/src/main/java/com/example/foodrecipes/data/repositories/RecipesRepository.kt
@@ -1,5 +1,6 @@
 package com.example.foodrecipes.data.repositories
 
+import com.example.foodrecipes.data.dtos.Recipe
 import com.example.foodrecipes.domain.RecipesDbDao
 import javax.inject.Inject
 
@@ -9,4 +10,12 @@ class RecipesRepository @Inject constructor(
     suspend fun getAllRecipes() = recipesDbDao.getAllRecipes()
 
     suspend fun getRecipesWithCategory(category: String) = recipesDbDao.getRecipesWithCategory(category)
+
+    suspend fun getRecipe(id: Int) = recipesDbDao.getRecipe(id)
+
+    suspend fun getFavoriteRecipes() = recipesDbDao.gerFavoriteRecipes()
+
+    suspend fun getFavoriteRecipesWithCategory(category: String) = recipesDbDao.getFavoriteRecipesWithCategory(category)
+
+    suspend fun insertRecipe(recipe: Recipe) = recipesDbDao.insertRecipe(recipe)
 }
--- app/src/main/java/com/example/foodrecipes/domain/RecipesDbDao.kt
+++ app/src/main/java/com/example/foodrecipes/domain/RecipesDbDao.kt
@@ -2,7 +2,9 @@ package com.example.foodrecipes.domain
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.foodrecipes.data.dtos.Recipe
 
 @Dao
@@ -16,7 +18,24 @@ interface RecipesDbDao {
         category: String
     ): List<Recipe>
 
+    @Query("SELECT * FROM recipes WHERE id == :id")
+    suspend fun getRecipe(
+        id: Int
+    ): Recipe
+
+    @Query("SELECT * FROM recipes WHERE isFavorite == 1")
+    suspend fun gerFavoriteRecipes(): List<Recipe>
+
+    @Query("SELECT * FROM recipes WHERE isFavorite == 1 AND categories = :category")
+    suspend fun getFavoriteRecipesWithCategory(
+        category: String
+    ): List<Recipe>
+
     @Insert
     suspend fun insertAll(recipes: List<Recipe>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipe(recipe: Recipe)
+
 
 }
